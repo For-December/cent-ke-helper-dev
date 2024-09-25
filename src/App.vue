07e117cea454ec19c0692254313ac5cc829fedbb
fordecember <1921567337@qq.com>
2024-09-25 15:30:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CourseCollapse from "./view/CourseCollapse.vue";
+</script>
+
+<template>
+<CourseCollapse />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/view/CourseCollapse.vue
@@ -0,0 +1,32 @@
+<template>
+  <n-collapse>
+    <template #header-extra>
+      <n-icon><CashIcon /></n-icon>
+    </template>
+    <template #arrow>
+      <n-icon>
+        <CashIcon />
+      </n-icon>
+    </template>
+    <n-collapse-item title="青铜" name="1">
+      <div>可以</div>
+    </n-collapse-item>
+    <n-collapse-item title="白银" name="2">
+      <div>很好</div>
+    </n-collapse-item>
+    <n-collapse-item title="黄金" name="3">
+      <div>真棒</div>
+    </n-collapse-item>
+  </n-collapse>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CashOutline as CashIcon } from '@vicons/ionicons5'
+
+export default defineComponent({
+  components: {
+    CashIcon
+  }
+})
+</script>
